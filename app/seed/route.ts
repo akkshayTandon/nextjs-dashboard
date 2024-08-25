2dfe13dcd5a97701389dd5d936b094dad9bbbc8f
akkshayat165 <76265879+akkshayTandon@users.noreply.github.com>
2024-08-26 02:17:17 +0530

    Chapter 11 Complete

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -101,22 +101,24 @@
 //   return insertedRevenue;
 // }
 
-// export async function GET() {
-//   // return Response.json({
-//   //   message:
-//   //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
-//   // });
-//   try {
-//     await client.sql`BEGIN`;
-//     await seedUsers();
-//     await seedCustomers();
-//     await seedInvoices();
-//     await seedRevenue();
-//     await client.sql`COMMIT`;
+export async function GET() {
+  return Response.json({
+    // message:
+    //   'Uncomment this file and remove this line. You can delete this file when you are finished.',
+    message:
+      'The above step was completed and the database has been seeded with the initial value. The requested file remains just for view purposes.'
+  });
+  //   try {
+  //     await client.sql`BEGIN`;
+  //     await seedUsers();
+  //     await seedCustomers();
+  //     await seedInvoices();
+  //     await seedRevenue();
+  //     await client.sql`COMMIT`;
 
-//     return Response.json({ message: 'Database seeded successfully' });
-//   } catch (error) {
-//     await client.sql`ROLLBACK`;
-//     return Response.json({ error }, { status: 500 });
-//   }
-// }
+  //     return Response.json({ message: 'Database seeded successfully' });
+  //   } catch (error) {
+  //     await client.sql`ROLLBACK`;
+  //     return Response.json({ error }, { status: 500 });
+  //   }
+}
